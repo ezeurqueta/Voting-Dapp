38a87c8d9a4b89bbc823d13bdf159f9dac4ee871
Ezequiel <eurqueta@technisys.com>
2022-05-16 21:05:35 -0300

    Initial commit, we created the foundations of the project by adding the pet shop turffle example

---
+++ contracts/Elections.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.5.16;
+
+contract Election {
+    //Store and Read candidates
+    string public candidate;
+    //Constructor
+
+    constructor() public {
+        string candidate = "Candidate 1";
+
+    }
+
+    
+}
