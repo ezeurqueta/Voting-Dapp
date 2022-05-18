e4cd911ef96dae2ff0e622cc5881f15c680c5d35
Ezequiel <eurqueta@technisys.com>
2022-05-18 20:27:50 -0300

    Deleted unused images and made a template preview of the final work

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,27 +2,32 @@
 pragma solidity 0.5.16;
 
 contract Election {
-    //Model a candidate
-    struct Candidate{
+    // Model a Candidate
+    struct Candidate {
         uint id;
         string name;
         uint voteCount;
     }
-    //Store candidate
-    mapping(uint => Candidate) candidates;
-    //Fetch candidate
-    //Store candidates count
+
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+    // Store Candidates
+    // Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+    // Store Candidates Count
     uint public candidatesCount;
 
-    constructor() public {
+    // voted event
+    event votedEvent (
+        uint indexed _candidateId
+    );
+
+    function Election () public {
         addCandidate("Candidate 1");
         addCandidate("Candidate 2");
     }
 
-    function addCandidate(string memory _name) private {
+    function addCandidate (string _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
-
-    
-}
