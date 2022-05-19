e5040e6280cfac6faf31cde997337df5266ae977
Ezequiel <eurqueta@technisys.com>
2022-05-18 21:22:28 -0300

    Finished process

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -17,17 +17,36 @@ contract Election {
     // Store Candidates Count
     uint public candidatesCount;
 
-    // voted event
     event votedEvent (
         uint indexed _candidateId
     );
 
-    function Election () public {
+    constructor () public {
         addCandidate("Candidate 1");
         addCandidate("Candidate 2");
     }
 
-    function addCandidate (string _name) private {
+    function addCandidate (string memory _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
+
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+
+        // trigger voted event
+        emit votedEvent(_candidateId);
+    }
+
+}    
